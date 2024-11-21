ba0cada5c92e73b7a312469fbfee01086e9f9535
hramses1 <arismendiramses@gmail.com>
2024-11-20 23:33:26 -0500

    Feature: peticion de api de pocketbase estado activo.

--- src/feature/chatbot/api/category_api.py
+++ src/feature/chatbot/api/category_api.py
@@ -1,7 +1,7 @@
 from common.axios import Axios
 
 def get_categories_api():
-    return Axios().get('/collections/categoria/records?perPage=-1&sort=-created')
+    return Axios().get('/collections/categoria/records?filter=activo=true')
 
 def get_category_api():
-    return Axios().get('/collections/categoria/records?perPage=-1&sort=-created')
+    return Axios().get('/collections/categoria/records?filter=activo=true')
